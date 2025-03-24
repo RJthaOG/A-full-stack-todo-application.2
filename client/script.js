0a69b369845db951994e8dc788c6f3809a158928
RJ <roderickw6@nycstudents.net>
2025-03-24 13:28:22 +0000

    Todo fixed

--- client/script.js
+++ client/script.js
@@ -1,38 +1,51 @@
-const todoForm = document.getElementById('todoForm');
-const todoInput = document.getElementById('todoInput');
-const todoList = document.getElementById('todoList');
-
+// Get DOM HTML Elements
+const form = document.getElementById("todoForm");
+const list = document.getElementById("todoList");
+const input = document.getElementById("todoInput");
+// Fetch all todos when page loads
 async function fetchTodos() {
   const url = "/api/todos";
   try {
-    const relay = await fetch(url);
-    if (!relay.ok) {
-      throw new Error(`Response status: ${relay.status}`);
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
     }
-
-    const todos = await relay.json();
+    const todos = await response.json();
+    console.log(todos);
     displayTodos(todos);
   } catch (error) {
     console.error(error.message);
   }
 }
 
+// Display todos in the list
 function displayTodos(todos) {
-  todoList.innerHTML = '';
-  todos.forEach(todo => {
-    const listItem = document.createElement('li');
-    listItem.textContent = todo.text;
-    todoList.appendChild(listItem);
+  let todoHTML = "";
+  todos.forEach((todo) => {
+    todoHTML += `
+             <li> ${todo.title} </li> 
+             `;
   });
+  list.innerHTML = todoHTML;
 }
 
-todoForm.addEventListener('submit', async (e) => {
+// Handle form submission
+todoForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const newTodo = {
-    text: todoInput.value
-  };
-  displayTodos([newTodo]);
-  todoInput.value = '';
+  try {
+    const response = await fetch("/api/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Automatically converted to "username=example&password=password"
+      body: JSON.stringify({ title: input.value }),
+    });
+    fetchTodos();
+  } catch (error) {
+    console.error(error.message);
+  }
 });
-
+// Load todos when page loads
 fetchTodos();
+// npm run dev
